f3a67ac7ec89121abbefe7a2bc8d19e17a1bdac4
ETHproductions <eth3792@nycap.rr.com>
2016-12-31 14:53:55 -0500

    Clean up bot-template.js

--- bot-template.js
+++ bot-template.js
@@ -2,27 +2,26 @@ function post(x, id) {
     document.getElementById('input').value = (id ? ":" + id + " ": "") + x;
     document.getElementById("sayit-button").click()
 }
-var last_message = "";
+var last_message = NaN;
 
 function f() {
+    // If any messages failed to send, resend them
     for (var i of document.getElementsByClassName("pending"))
         for (var j of i.children)
             for (var k of j.children)
                 if (k.innerHTML == "retry") k.click();
+    
+    var e = [].slice.call(document.getElementsByClassName("content"), -1)[0]; // message element
+    var a = e.textContent; // text of the message
+    var message_id = e.parentElement.id.slice(8); // id of the message
+    var username = e.parentElement.parentElement.parentElement.querySelector(".signature .username").textContent; // user who posted
+    
+    // We don't want to do anything if no new messages have been posted
+    if (e === last_message) return;
+    last_message = e;
 
-    var e = [].slice.call(document.getElementsByClassName("message")).slice(-1)[0];
-    console.log(e.children);
-    var a = [].slice.call(document.getElementsByClassName("content")).slice(-1)[0].textContent;
-    if (a == last_message) return;
-    last_message = a;
-
-    var username = "Feeds";
-    for (var i of document.getElementsByClassName("username")) username = i.innerHTML;
-    var message_id = e.id.match(/\d+/).slice(-1)[0];
-
-    console.log(a);
-    if (/@/.test(a) && !/@ETHbot/i.test(a)) return; // Avoid messages that ping other users
-    if (username == "ETHbot") return; // Avoid your own messages
+    if (/@/.test(a) && !/@ETHbot/i.test(a)) return; // Avoid messages directed at other users
+    if (username === "ETHbot") return; // Avoid your own messages
  
     var y = "Your message here";
  
